2edc7430bec6a2b44b81ca12c49f2c9969e04e6a
IKAH Design <99662370+MyIKAHDesign@users.noreply.github.com>
2023-01-30 17:12:16 +0200

    Header as a component

--- pages/work-with-me.tsx
+++ pages/work-with-me.tsx
@@ -1,9 +1,11 @@
 
-export default function WorkWithMe(){
+export default function WorkWithMe() {
     return (
-        <div>
+        <div className="isolate bg-white">
+            <Header />
             <h1>Work with me</h1>
             <p>Work with me.</p>
         </div>
+
     );
 }
