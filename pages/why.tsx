55348ce9f788f69382687e235c81692af5d5729d
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2023-01-30 22:28:38 +0200

    I added the why page

---
+++ pages/why.tsx
@@ -0,0 +1,17 @@
+export default function Reason() {
+  return (
+    <div>
+        <div>
+            <button href="/">Back</button>
+        </div>
+      <div>
+        <h1>Why I made this website?</h1>
+        <p>
+          As I said on the home page, I am buiding many website because I am
+          working very hard to help my mother language Kifuliiru. Everything I
+          can do, I'm doing it for that purpose.
+        </p>
+      </div>
+    </div>
+  );
+}
