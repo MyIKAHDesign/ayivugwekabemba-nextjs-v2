dfca79e0deadfcf87e6afddc8e3fc5d53e5f545f
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2023-01-30 22:31:13 +0200

    Reediting the vhy page

--- pages/why.tsx
+++ pages/why.tsx
@@ -1,17 +1,14 @@
 export default function Reason() {
   return (
     <div>
-        <div>
-            <button href="/">Back</button>
-        </div>
-      <div>
-        <h1>Why I made this website?</h1>
-        <p>
-          As I said on the home page, I am buiding many website because I am
-          working very hard to help my mother language Kifuliiru. Everything I
-          can do, I'm doing it for that purpose.
-        </p>
-      </div>
+      <button href="/">Back</button>
+      <br />
+      <h1>Why I made this website?</h1>
+      <p>
+        As I said on the home page, I am buiding many website because I am
+        working very hard to help my mother language Kifuliiru. Everything I can
+        do, I'm doing it for that purpose.
+      </p>
     </div>
   );
 }
