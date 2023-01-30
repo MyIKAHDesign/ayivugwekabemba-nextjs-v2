19270aa5c806f6d8056ea44f922ba594306df9d4
IKAH Design <99662370+MyIKAHDesign@users.noreply.github.com>
2023-01-30 17:17:46 +0200

    Import the Header from work with me

--- pages/work-with-me.tsx
+++ pages/work-with-me.tsx
@@ -1,8 +1,9 @@
+import Header from "../app/header"
 
 export default function WorkWithMe() {
     return (
         <div className="isolate bg-white">
-            <Header />
+            <Header/>
             <h1>Work with me</h1>
             <p>Work with me.</p>
         </div>
