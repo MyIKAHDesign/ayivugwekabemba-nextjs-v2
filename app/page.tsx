dc892d7ad614a725b83f4f4cb43d6cf7d960bc69
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2023-01-30 22:34:25 +0200

    Some changes

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
           <div className="mx-auto max-w-2xl py-16 sm:py-30 lg:py-30">
             <div className="hidden sm:mb-8 sm:flex sm:justify-center">
               <div className="relative rounded-full py-1 px-3 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                This website is under construction. <a href="/why" className="font-semibold text-indigo-600"><span className="absolute inset-0" aria-hidden="true"></span>Read more <span aria-hidden="true">&rarr;</span></a>
+                This website is under construction. <a href="/" className="font-semibold text-indigo-600"><span className="absolute inset-0" aria-hidden="true"></span>Read more <span aria-hidden="true">&rarr;</span></a>
               </div>
             </div>
             <div className="text-center">
--- pages/why.tsx
+++ pages/why.tsx
@@ -1,14 +1,35 @@
 export default function Reason() {
   return (
-    <div>
-      <button href="/">Back</button>
-      <br />
-      <h1>Why I made this website?</h1>
-      <p>
-        As I said on the home page, I am buiding many website because I am
-        working very hard to help my mother language Kifuliiru. Everything I can
-        do, I'm doing it for that purpose.
-      </p>
+    <div className="isolate bg-white">
+      {/* <Header/> */}
+      <main>
+        <div className="relative px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl py-16 sm:py-30 lg:py-30">
+            <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+              <div className="relative rounded-full py-1 px-3 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                Go back. <a href="/" className="font-semibold text-indigo-600"><span className="absolute inset-0" aria-hidden="true"></span>Home <span aria-hidden="true">&rarr;</span></a>
+              </div>
+            </div>
+            <div className="text-center">
+              <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Ayivugwe Kabemba</h1>
+              <p className="mt-6 text-lg leading-8 text-gray-600">Welcome. Thank you for visiting my website. I love building websites when it's possible to me.
+              </p>
+              <p className="mt-6 text-lg leading-8 text-gray-600">
+                The reason why I am building so many websites is my mother language is called <span className="text-red">Kifuliiru</span>. It's one of the laguages with limited resources online. I'm doing everything I can to reduce that gap.
+                That's why my main websites are in Kifuliiru. Interested in visiting them? Links are here bellow. Thank you!
+              </p>
+              <div className="mt-10 flex items-center justify-center gap-x-6">
+                <a href="https://ayivugwe.editorx.io/ayivugwekabemba" target="_blank" className="rounded-md bg-indigo-600 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">More about me</a>
+                <a href="https://ibufuliiru.editorx.io/ibufuliiru" target="_blank" className="text-base font-semibold leading-7 text-gray-900">Learn more <span aria-hidden="true">→</span></a>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+      </main>
+
+
     </div>
+
   );
 }
