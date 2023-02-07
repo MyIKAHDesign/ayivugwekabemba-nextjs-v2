c8b7bfc029cfcf1b15f7a3a4bd8026ce0d5d6beb
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2023-02-07 17:10:49 +0200

    I commented the code I added

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
            
 
           </div>
-          <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+         {/*  <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
               <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/ayivugwe.png" alt="" width="384" height="512" />
               <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
                 <blockquote>
@@ -56,7 +56,7 @@ export default function Home() {
                   </div>
                 </figcaption>
               </div>
-            </figure>
+            </figure> */}
         </div>
       </main>
     </div>
