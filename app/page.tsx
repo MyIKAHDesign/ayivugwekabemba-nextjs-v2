ec60b68f45d36a6a392ef516c2aa8f91d1f1ddb3
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2023-02-07 15:51:46 +0200

    Nahindula i size ye text mbamu

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <main>
         <div className="relative px-6 lg:px-8">
           <div className="text-center">
-            <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-6xl"> <a href="/"> Ayivugwe Kabemba </a></h1>
+            <h1 className="text-xl font-bold tracking-tight text-gray-900 sm:text-xl"> <a href="/"> Ayivugwe Kabemba </a></h1>
           </div>
           <div className="mx-auto max-w-2xl py-16 sm:py-30 lg:py-30">
             <div className="hidden sm:mb-8 sm:flex sm:justify-center">
@@ -21,7 +21,7 @@ export default function Home() {
               </div>
             </div>
             <div className="text-center">
-             <p className="mt-6 text-lg leading-8 text-gray-600">Welcome. Thank you for visiting my website. Continue reading to know more. I love building websites when it's possible to me. Note that this is not all what I have to show you. 
+             <p className="mt-6 text-lg leading-8 text-gray-600">Welcome. Thank you for visiting my website. Note that this is not all what I have to show you. Continue reading to know more. I love building websites when it's possible to me.  
               </p>
               <p className="mt-6 text-lg leading-8 text-gray-600">
                 The reason why I am building so many websites is my mother language is called <span className="text-red">Kifuliiru</span>. It's one of the laguages with limited resources online. I'm doing everything I can to reduce that gap.
