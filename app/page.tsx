e063e45509506457fbd5ece379b64751c23a1118
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2023-02-07 15:32:09 +0200

    Nayongera i link ku Logo

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,9 @@ export default function Home() {
       {/* <Header/> */}
       <main>
         <div className="relative px-6 lg:px-8">
+          <div className="text-center">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl"> <a href="/"> Ayivugwe Kabemba </a></h1>
+          </div>
           <div className="mx-auto max-w-2xl py-16 sm:py-30 lg:py-30">
             <div className="hidden sm:mb-8 sm:flex sm:justify-center">
               <div className="relative rounded-full py-1 px-3 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
@@ -31,7 +34,7 @@ export default function Home() {
               </div>
             </div>
           </div>
-          
+
         </div>
       </main>
 
