66553a7d404c20eca3a9838f2620cc7e7658cd95
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2023-02-10 15:04:52 +0200

    Some other changes

--- app/page.tsx
+++ app/page.tsx
@@ -12,10 +12,10 @@ export default function Home() {
       <main>
         <div className="relative px-6 lg:px-8">
           <div className="mx-auto max-w-2xl py-16 sm:py-30 lg:py-30">
-            <div className="hidden sm:mb-8 sm:flex sm:justify-center text-center">
+            <div className=" sm:mb-8 sm:flex sm:justify-center text-center">
               <h1 className="text-4xl text-sm font-bold tracking-tight text-gray-900 sm:text-4xl"> <a href="/"> Ayivugwe Kabemba </a></h1>
             </div>
-            <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+            <div className=" sm:mb-8 sm:flex sm:justify-center">
               <div className="relative rounded-full py-1 px-3 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
                 This website is under construction. <a href="/" className="font-semibold text-indigo-600"><span className="absolute inset-0" aria-hidden="true"></span>Read more <span aria-hidden="true">&rarr;</span></a>
               </div>
