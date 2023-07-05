359995a8ad7b5963e7d1f62199dffa8fd1c4be8a
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2023-07-05 18:01:51 +0200

    Update layout.tsx
    add favicon to the app

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,9 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
+      <head >
+        <link rel='icon' href='/Ayiv.png'/>
+      </head>
       <body>{children}</body>
     </html>
   )
