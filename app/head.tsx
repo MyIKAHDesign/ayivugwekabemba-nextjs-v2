17b5694279fd90e6752d2e08b2ac69bd6ef97bff
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2023-07-10 12:39:09 +0200

    changes in Head

--- app/head.tsx
+++ app/head.tsx
@@ -5,8 +5,7 @@ export default function Head() {
       <title>Ayivugwe Kabemba</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Ayivugwe Kabemba Mukome. Namuyehuulo nakola mu NextJs na TailwindCSS" />
-      <link rel="icon" href="/favicon.ico" />
-      <br/>
+      <link rel="icon" href="/ayiv.ico" />
     </>
   )
 }
