28abb92767508aa460c4273fe2da1acbf93317da
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2024-02-13 01:26:10 -0800

    solve Error: ' can be escaped with &apos;

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-              Welcome. I'm Ayivugwe Kabemba Mukome.
+              Welcome. I&lsquo;m Ayivugwe Kabemba Mukome.
             </p>
           </a>
 
@@ -140,7 +140,7 @@ export default function Home() {
               </span>
             </h2>
             <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-              This is my other media agency in Kifuliiru that I created. It's still
+              This is my other media agency in Kifuliiru that I created. It&lsquo;s still
               under development.
             </p>
           </a>
