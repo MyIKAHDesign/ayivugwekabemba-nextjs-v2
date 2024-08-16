8624db98820cfc870028272c7702e53fefc38582
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2024-08-16 01:33:55 -0700

    change styles and add text

--- app/page.tsx
+++ app/page.tsx
@@ -16,11 +16,23 @@ export default function Home() {
           </a>
 
           <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+            <p
+              className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+             
+              rel="noopener noreferrer"
+              style={{ fontSize: '18px' }} // Adjust font size as needed
+            >
+              Visit my Kifuliiru website
+            </p>
+          </div>
+
+          <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
             <a
               className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
               href="https://ayivugwe.editorx.io/ayivugwekabemba/"
               target="_blank"
               rel="noopener noreferrer"
+              style={{ fontSize: '16px' }} // Adjust font size as needed
             >
               Visit my Kifuliiru website
             </a>
