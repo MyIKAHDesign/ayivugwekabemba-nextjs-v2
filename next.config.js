bf5e0f54fd9e53c5919b57f82a5aa9df3bbcb3e5
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2024-09-03 11:00:26 -0700

    fix Add animations on the background and new theme

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Remove the experimental section if it only contained appDir
+  // If you have other experimental features, keep them but remove appDir
   experimental: {
-    appDir: true,
+    // other experimental features can stay here
   },
+  // other config options...
 }
 
 module.exports = nextConfig
