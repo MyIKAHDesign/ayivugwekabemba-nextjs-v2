cfd66293e1c3c84446de40e93ddd32a0d8aabe06
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2024-09-04 02:39:10 -0700

    Remove resume

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -22,12 +22,7 @@ export default function Footer() {
                   <span className="absolute left-0 bottom-[-2px] w-full h-[2px] bg-[#F0F4C3] transform scale-x-0 transition-transform duration-300 group-hover:scale-x-100"></span>
                 </Link>
               </li>
-              <li>
-                <Link href="/resume" className="relative text-white transition-colors duration-300 hover:text-[#F0F4C3] group">
-                  My Resumé
-                  <span className="absolute left-0 bottom-[-2px] w-full h-[2px] bg-[#F0F4C3] transform scale-x-0 transition-transform duration-300 group-hover:scale-x-100"></span>
-                </Link>
-              </li>
+             
               {/* Add more links as needed */}
             </ul>
           </nav>
