93067b6b9313cc512fb58b81a87be0ac16bfac0a
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2024-09-03 21:17:41 -0700

    Add resume

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -17,6 +17,12 @@ export default function Header() {
               <span className="absolute left-0 bottom-[-2px] w-full h-[2px] bg-[#0A21C0] transform scale-x-0 transition-transform duration-300 group-hover:scale-x-100"></span>
             </Link>
           </li>
+          <li>
+            <Link href="/resume" className="relative text-[#0A21C0] transition-colors duration-300 hover:text-[#050A44] group">
+              Resume
+              <span className="absolute left-0 bottom-[-2px] w-full h-[2px] bg-[#0A21C0] transform scale-x-0 transition-transform duration-300 group-hover:scale-x-100"></span>
+            </Link>
+          </li>
           {/* Add more links as needed */}
         </ul>
       </nav>
