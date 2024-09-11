987d1df042d97108ca7c7c6c184555afeae4e293
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2024-09-11 01:59:17 -0700

    Change links for Imyazi and Radio Ibufuliiru

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -16,26 +16,66 @@ export default function Home() {
         </div>
         <div className="md:w-2/3 md:pl-8">
           <h1 className="text-4xl font-bold mb-4">Ayivugwe Kabemba Mukome</h1>
-          <p className="text-base italic text-gray-600 mb-4">Project Manager, Software Developer & Kifuliiru Language Advocate</p>
-          <p className="text-base text-gray-600 mb-4">I am a Google-certified Project Manager and Software Developer, passionate about creating innovative solutions. As a dedicated advocate for the Kifuliiru language, I strive to empower communities through technology and cultural preservation.</p>
+          <p className="text-base italic text-gray-600 mb-4">
+            Project Manager, Software Developer & Kifuliiru Language Advocate
+          </p>
+          <p className="text-base text-gray-600 mb-4">
+            I am a Google-certified Project Manager and Software Developer,
+            passionate about creating innovative solutions. As a dedicated
+            advocate for the Kifuliiru language, I strive to empower communities
+            through technology and cultural preservation.
+          </p>
         </div>
       </section>
 
       <section className="mb-12">
-        <h2 className="text-3xl font-semibold mb-6 text-[#0A21C0]">My Projects</h2>
+        <h2 className="text-3xl font-semibold mb-6 text-[#0A21C0]">
+          My Projects
+        </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           {[
-            { title: "Ayivugwe Kabemba website", url: "https://ayivugwe.editorx.io/ayivugwekabemba/", description: "My main website in Kifuliiru, the language I love the most." },
-            { title: "IKAH Design", url: "https://ikahdesign.editorx.io/ikah", description: "My Design Agency" },
-            { title: "Ibufuliiru", url: "https://ibufuliiru.editorx.io/ibufuliiru", description: "My main project currently." },
-            { title: "Kifuliiru", url: "https://ibufuliiru.editorx.io/kifuliiru", description: "My project about the Kifuliiru language." },
-            { title: "Imyazi mu Kifuliiru", url: "https://ibufuliiru.editorx.io/imyazi", description: "A media agency in Kifuliiru for creating digital content." },
-            { title: "Radio Ibufuliiru", url: "https://ibufuliiru.editorx.io/imyazi", description: "Another media agency in Kifuliiru (under development)." },
+            {
+              title: "Ayivugwe Kabemba website",
+              url: "https://ayivugwe.editorx.io/ayivugwekabemba/",
+              description:
+                "My main website in Kifuliiru, the language I love the most.",
+            },
+            {
+              title: "IKAH Design",
+              url: "https://ikahdesign.editorx.io/ikah",
+              description: "My Design Agency",
+            },
+            {
+              title: "Ibufuliiru",
+              url: "https://ibufuliiru.editorx.io/ibufuliiru",
+              description: "My current main project.",
+            },
+            {
+              title: "Kifuliiru",
+              url: "https://ibufuliiru.editorx.io/kifuliiru",
+              description: "My project about the Kifuliiru language.",
+            },
+            {
+              title: "Imyazi mu Kifuliiru",
+              url: "https://imyazi.substack.com/",
+              description:
+                "A media agency in Kifuliiru for creating digital content.",
+            },
+            {
+              title: "Radio Ibufuliiru",
+              url: "https://radioibufuliiru.substack.com/",
+              description: "My other media agency in Kifuliiru.",
+            },
           ].map((project, index) => (
-            <div key={index} className="bg-white p-6 rounded-lg shadow-md transition-all duration-300 hover:shadow-xl hover:scale-105 hover:bg-[#feffed]">
-              <h3 className="text-xl font-semibold mb-2 text-[#0A21C0]">{project.title}</h3>
+            <div
+              key={index}
+              className="bg-white p-6 rounded-lg shadow-md transition-all duration-300 hover:shadow-xl hover:scale-105 hover:bg-[#feffed]"
+            >
+              <h3 className="text-xl font-semibold mb-2 text-[#0A21C0]">
+                {project.title}
+              </h3>
               <p className="mb-4 text-[#141619]">{project.description}</p>
-              <a 
+              <a
                 href={project.url}
                 className="inline-block px-4 py-2 bg-[#0A21C0] text-white rounded hover:bg-[#050A44] transition-colors duration-300"
                 target="_blank"
