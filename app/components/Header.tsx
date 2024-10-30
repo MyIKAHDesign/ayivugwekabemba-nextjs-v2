c33fec17f127c1020a835100f2963e0fc9ef750b
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2024-10-30 10:57:34 -0700

    Add New version with localstorage for theme and correct mobile menu height

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -119,14 +119,16 @@ const Header = () => {
         {/* Mobile Navigation Menu */}
         {isMenuOpen && (
           <div
-            className={`md:hidden absolute top-full left-0 right-0 transition-all duration-300 max-h-64 overflow-hidden ${
+            className={`md:hidden absolute top-full left-0 right-0 transition-all duration-300 max-h-70 overflow-hidden ${
               darkMode
                 ? "bg-black/95 border-b border-slate-900"
                 : "bg-white/80 border-b border-slate-200"
             } backdrop-blur-md`}
           >
-            <div className="px-4 py-2 space-y-1">
-              {navLinks.map((link) => (
+            <div className="px-4 py-2 space-y-1 pb-12">
+              {" "}
+              {/* Increased padding-bottom */}
+              {navLinks.map((link, index) => (
                 <button
                   key={link.name}
                   onClick={() => handleNavigation(link.href)}
@@ -134,7 +136,7 @@ const Header = () => {
                     darkMode
                       ? "text-slate-500 hover:text-slate-200 hover:bg-slate-900/90"
                       : "text-slate-600 hover:text-slate-900 hover:bg-slate-100"
-                  }`}
+                  } ${index === navLinks.length - 1 ? "mb-10" : ""}`}
                 >
                   {link.name}
                 </button>
