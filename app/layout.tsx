74d63db99a574b3949d606d8972ffb812967888e
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2024-11-04 15:12:46 -0800

    Remove Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
           <Header />
           <main className="flex-1">{children}</main>
           <Footer />
-          <Analytics />
+          {/* <Analytics /> */}
         </ThemeProvider>
       </body>
     </html>
